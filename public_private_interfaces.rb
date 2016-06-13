a39fc99cde2c3bb6d8be7e70d35ef762b0b05807
ks2355244 <ks2355244@yahoo.com>
2016-06-13 15:53:10 +0800

    1st edit

--- public_private_interfaces.rb
+++ public_private_interfaces.rb
@@ -1,1 +1,25 @@
 # Place your solutions here
+class BankAccount
+
+	def initialize(customer_name, type, acct_number)
+		@customer_name = customer_name
+		@type = type
+		@acct_number = acct_number
+	end
+
+	public
+	def name 
+	 "#{@customer_name}, #{@type}, #{@acct_number}"
+	end
+
+	private
+	def hide_acct
+		acct_number = @acct_number.gsub(/^\d{3}\W\d{2}/,"*****")
+	end
+end	
+
+my_acct = BankAccount.new("Junipero", "Checking", "347-923-239")
+
+puts "My account information is #{my_acct.name}."
+
+
